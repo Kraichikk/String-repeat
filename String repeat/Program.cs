ea6b2b1f69c496164d5b025a75454112f7b21bc2
Kraichikk <108270237+Kraichikk@users.noreply.github.com>
2023-02-24 11:56:48 +0200

    Add project files.

---
+++ String repeat/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace String_repeat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static string RepeatStr(int n, string s)
+        {
+            string val = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                val += s;
+            }
+            return val;
+        }
+    }
+}
